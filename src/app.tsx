4e3033df6592691cac42660ed4dd4360ec727908
cavj83 <l23210559@tectijuana.edu.mx>
2024-05-17 11:16:50 -0700

    up card home 	modified:   src/app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,8 @@ import { Header } from "@/components/layout";
 import { withProviders } from "@/app/hocs";
 import { useWalletSync } from "@/features/wallet/hooks";
 import { Home } from "./pages/home";
-
+import { NavBar } from "./pages/home/navbar";
+import { Card, CardHeader, CardBody, CardFooter, Stack, Heading, Text } from '@chakra-ui/react'
 function Component() {
   const { isApiReady } = useApi();
   const { isAccountReady } = useAccount();
@@ -16,9 +17,19 @@ function Component() {
 
   return (
     <>
-      <Header isAccountVisible={isAccountReady}/>
-      {isAppReady ? <Home /> : <ApiLoader />}
-      </>
+      <Card key="Home" size="sm">
+        <CardHeader>
+          <Header isAccountVisible={isAccountReady}/>
+        </CardHeader>
+        <CardBody>
+            {isAppReady ? <Home /> : <ApiLoader />}
+        </CardBody>
+        <CardFooter>
+          <Heading size="lg">Stay Conneted</Heading>
+          {<img id="imglogo"  className="img-fluid img-thumbnail" src="./src\img\Slogan\Eslogan2.png"/>}
+        </CardFooter>
+      </Card>
+    </>
   );
 }
 
