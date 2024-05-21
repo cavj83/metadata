afbffcf390b986a6ac2ce3f81cb92121e852fd0a
angelrpi4 <aelr.lara@hotmail.com>
2024-05-20 17:44:36 -0700

    Removed previous template with a refresh

---
+++ backend/fabricator/target/debug/deps/fabricator-907bae9f2b68f473.d
@@ -0,0 +1,7 @@
+/home/angel/metadata/backend/fabricator/target/debug/deps/libfabricator-907bae9f2b68f473.rmeta: src/lib.rs
+
+/home/angel/metadata/backend/fabricator/target/debug/deps/libfabricator-907bae9f2b68f473.rlib: src/lib.rs
+
+/home/angel/metadata/backend/fabricator/target/debug/deps/fabricator-907bae9f2b68f473.d: src/lib.rs
+
+src/lib.rs:
