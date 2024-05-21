b6f801d6b7af1446db655e533f6147e74626010c
angelrpi4 <aelr.lara@hotmail.com>
2024-05-20 17:44:36 -0700

    Removed previous template with a refresh

---
+++ backend/fabricator/target/debug/deps/fabricator-77d80883e4b38f94.d
@@ -0,0 +1,5 @@
+/home/angel/metadata/backend/fabricator/target/debug/deps/fabricator-77d80883e4b38f94: src/lib.rs
+
+/home/angel/metadata/backend/fabricator/target/debug/deps/fabricator-77d80883e4b38f94.d: src/lib.rs
+
+src/lib.rs:
