8de86d0611247d35dc316806ce90f46a2d68d0d7
angelrpi4 <aelr.lara@hotmail.com>
2024-05-21 22:37:46 -0700

    Fabricator Template working, pending NFT

--- backend/fabricator/target/debug/deps/fabricator-77d80883e4b38f94.d
+++
@@ -1,5 +0,0 @@
-/home/angel/metadata/backend/fabricator/target/debug/deps/fabricator-77d80883e4b38f94: src/lib.rs
-
-/home/angel/metadata/backend/fabricator/target/debug/deps/fabricator-77d80883e4b38f94.d: src/lib.rs
-
-src/lib.rs:
--- backend/fabricator/target/debug/deps/fabricator-907bae9f2b68f473.d
+++
@@ -1,7 +0,0 @@
-/home/angel/metadata/backend/fabricator/target/debug/deps/libfabricator-907bae9f2b68f473.rmeta: src/lib.rs
-
-/home/angel/metadata/backend/fabricator/target/debug/deps/libfabricator-907bae9f2b68f473.rlib: src/lib.rs
-
-/home/angel/metadata/backend/fabricator/target/debug/deps/fabricator-907bae9f2b68f473.d: src/lib.rs
-
-src/lib.rs:
